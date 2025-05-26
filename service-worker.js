25c09b1bdee5bc41fb90307e6d2b4102d353a6e2
RehanAldi <rehannn12e@gmail.com>
2025-05-26 12:52:16 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,9 +6,10 @@ const urlsToCache = [
   "./script.js",
   "./manifest.json",
   "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icons/icon-512.png"
 ];
 
+// Install service worker dan cache semua resource penting
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -17,10 +18,26 @@ self.addEventListener("install", (event) => {
   );
 });
 
+// Intercept request dan coba ambil dari cache, kalau tidak ada baru fetch dari network
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
+  );
+});
+
+// Optional: bisa tambah activate event untuk membersihkan cache lama (untuk update versi)
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
   );
 });
